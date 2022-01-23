aecfc31c5f376981e46fe0f8abe351cc80013240
Marfnl <46224310+Marfnl@users.noreply.github.com>
2022-01-23 23:22:18 +0100

    Update selenium.py
    Work in progress!
    Trying to Added detection prevention from:
    __author__ = "Soumil Shah"
    __email__ = "shahsoumil519@gmail.com"

--- kora/selenium.py
+++ kora/selenium.py
@@ -1,31 +1,113 @@
-# install chromium, its driver, and selenium
-import os
-os.system('apt update')
-os.system('apt install chromium-chromedriver')
-os.system('pip install selenium')
-# set options to be headless, ..
-from selenium import webdriver
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-# create a webdriver instance, ready to use
-wd = webdriver.Chrome('chromedriver',options=options)
+__author__ = "Soumil Shah"
+__email__ = "shahsoumil519@gmail.com"
+try:
 
-# make it easier to query and explore elements
-wd.select = wd.find_elements_by_css_selector
-wd.select1 = wd.find_element_by_css_selector
-from selenium.webdriver.remote.webelement import WebElement
-WebElement.__str__ = lambda self: self.get_attribute('outerHTML')
-WebElement.__repr__ = WebElement.__str__
-WebElement.select = WebElement.find_elements_by_css_selector
-WebElement.select1 = WebElement.find_element_by_css_selector
-WebElement.__getitem__ = WebElement.get_attribute
+    import sys
+    import os
 
-# show screenshot easily with _repr_png_
-def _screen_shot(self):
-    from tempfile import NamedTemporaryFile as TempFile
-    tmp = TempFile(suffix='.png')
-    self.save_screenshot(tmp.name)
-    return tmp.read()
-webdriver.Chrome._repr_png_ = _screen_shot
+    from fp.fp import FreeProxy
+    from fake_useragent import UserAgent
+    from bs4 import BeautifulSoup
+    from selenium import webdriver
+    from selenium.webdriver import Chrome
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.common.action_chains import ActionChains
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.common.exceptions import TimeoutException
+    import time
+    print('all module are loaded ')
+
+except Exception as e:
+
+    print("Error ->>>: {} ".format(e))
+
+
+class Spoofer(object):
+
+    def __init__(self, country_id=['US'], rand=True, anonym=True):
+        self.country_id = country_id
+        self.rand = rand
+        self.anonym = anonym
+        self.userAgent, self.ip = self.get()
+
+    def get(self):
+        ua = UserAgent()
+        proxy = FreeProxy(country_id=self.country_id, rand=self.rand, anonym=self.anonym).get()
+        ip = proxy.split("://")[1]
+        return ua.random, ip
+
+
+class DriverOptions(object):
+
+    def __init__(self):
+
+        self.options = Options()
+        self.options.add_argument('--no-sandbox')
+        self.options.add_argument('--start-maximized')
+        self.options.add_argument('--start-fullscreen')
+        self.options.add_argument('--single-process')
+        self.options.add_argument('--disable-dev-shm-usage')
+        self.options.add_argument("--incognito")
+        self.options.add_argument('--disable-blink-features=AutomationControlled')
+        self.options.add_argument('--disable-blink-features=AutomationControlled')
+        self.options.add_experimental_option('useAutomationExtension', False)
+        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        self.options.add_argument("disable-infobars")
+
+        self.helperSpoofer = Spoofer()
+
+        self.options.add_argument('user-agent={}'.format(self.helperSpoofer.userAgent))
+        self.options.add_argument('--proxy-server=%s' % self.helperSpoofer.ip)
+
+
+class WebDriver(DriverOptions):
+
+    def __init__(self, path=''):
+        DriverOptions.__init__(self)
+        self.driver_instance = self.get_driver()
+
+    def get_driver(self):
+
+        print("""
+        IP:{}
+        UserAgent: {}
+        """.format(self.helperSpoofer.ip, self.helperSpoofer.userAgent))
+
+        PROXY = self.helperSpoofer.ip
+        webdriver.DesiredCapabilities.CHROME['proxy'] = {
+            "httpProxy":PROXY,
+            "ftpProxy":PROXY,
+            "sslProxy":PROXY,
+            "noProxy":None,
+            "proxyType":"MANUAL",
+            "autodetect":False
+        }
+        webdriver.DesiredCapabilities.CHROME['acceptSslCerts'] = True
+
+        path = os.path.join(os.getcwd(), '../windowsDriver/chromedriver.exe')
+
+        driver = webdriver.Chrome(executable_path=path, options=self.options)
+        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source":
+                "const newProto = navigator.__proto__;"
+                "delete newProto.webdriver;"
+                "navigator.__proto__ = newProto;"
+        })
+
+        return driver
+
+
+def main():
+
+    driver= WebDriver()
+    driverinstance = driver.driver_instance
+    driverinstance.get("https://www.expressvpn.com/what-is-my-ip")
+    time.sleep(5)
+    print("done")
+
+if __name__ == "__main__":
+    main()
