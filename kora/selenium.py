d49db222421258e3117644c78269d10f29eb5ee2
Marfnl <46224310+Marfnl@users.noreply.github.com>
2022-02-07 23:42:03 +0100

    Update selenium.py

--- kora/selenium.py
+++ kora/selenium.py
@@ -8,26 +8,15 @@ os.system('pip install selenium')
 import selenium
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 print('all module are loaded ')
 
 # --- webdriver class that can be closed and restarted ---
-
-class By(object):
-#'''Set of supported locator strategies.'''
-    ID = "id"
-    XPATH = "xpath"
-    LINK_TEXT = "link text"
-    PARTIAL_LINK_TEXT = "partial link text"
-    NAME = "name"
-    TAG_NAME = "tag name"
-    CLASS_NAME = "class name"
-    CSS_SELECTOR = "css selector"
 
 class WD(webdriver.Chrome):
     ''' webdriver helper class'''
@@ -46,16 +35,16 @@ class WD(webdriver.Chrome):
 
     # make it easier to query and explore elements 
     def select(self, *args, **kwargs):
-        return self.find_elements_by_css_selector(*args, **kwargs)
+        return self.find_elements(By.CSS_SELECTOR,*args, **kwargs)
 
     def select1(self, *args, **kwargs):
-        return self.find_element_by_css_selector(*args, **kwargs)
+        return self.find_element(By.CSS_SELECTOR,*args, **kwargs)
    
 
-# show screenshot easily with _repr_png_
-#def _screen_shot(self):
-#    from tempfile import NamedTemporaryFile as TempFile
-#    tmp = TempFile(suffix='.png')
-#    self.save_screenshot(tmp.name)
-#    return tmp.read()
-#webdriver.Chrome._repr_png_ = _screen_shot
+ show screenshot easily with _repr_png_
+def _screen_shot(self):
+    from tempfile import NamedTemporaryFile as TempFile
+    tmp = TempFile(suffix='.png')
+    self.save_screenshot(tmp.name)
+    return tmp.read()
+webdriver.Chrome._repr_png_ = _screen_shot
