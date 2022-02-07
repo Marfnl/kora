ca9701fbc7c10def4f4ced97496b997850e9b786
Marfnl <46224310+Marfnl@users.noreply.github.com>
2022-02-07 22:06:38 +0100

    Update selenium.py

--- kora/selenium.py
+++ kora/selenium.py
@@ -19,7 +19,7 @@ print('all module are loaded ')
 
 class WD(webdriver.Chrome):
     ''' webdriver helper class'''
-    By = By    
+        wd = WD()    
     def __init__(self):
         '''instantiate driver'''
         
