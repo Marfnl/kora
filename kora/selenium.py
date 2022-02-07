55d0f64b2fd10f90918143e96dc7ff612e2c5efb
Marfnl <46224310+Marfnl@users.noreply.github.com>
2022-02-07 22:40:59 +0100

    Update selenium.py

--- kora/selenium.py
+++ kora/selenium.py
@@ -9,7 +9,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 # from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.common.action_chains import ActionChains
 # from selenium.webdriver.support import expected_conditions as EC
