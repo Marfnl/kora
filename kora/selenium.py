5d734f8b1e01723661babb2e0ab83d02b2e1071b
Marfnl <46224310+Marfnl@users.noreply.github.com>
2022-02-15 12:32:13 +0100

    Update selenium.py
    I have updated to run wd in a class,
    this way wd.quit() works fine
    
    Sample code:
    
    !pip install git+git://github.com/Marfnl/kora.git -q
    from kora.selenium import WD,By,Keys,WebDriverWait,EC
    from PIL import Image
    
    # Create a new instance of the ChromeDriver Driver
    wd = WD()
    
    #make the instance go to: https://google.com
    wd.get("https://google.com")
    
    
    
    #Finds search bar using XPATCH. and types: Selenium website test using .send_keys('Selenium website test')
    searchbar = wd.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('Selenium website test')
    
    #Finds Google Search button using CSS_SELECTOR. and submits it
    click_button = wd.find_element(By.CSS_SELECTOR,('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b')).submit()
    
    #Takes a screenshot from the page
    screenshot = wd.save_screenshot("screenshot1.png")
    
    #Opens the screenshot for screenshot1.png for use as output.
    display_screenshot = Image.open("screenshot1.png")
    
    #Displays the screenshot for viewing in the output 
    display(display_screenshot)
    
    #stops the webdriver
    wd.quit()

--- kora/selenium.py
+++ kora/selenium.py
@@ -1,31 +1,59 @@
-# install chromium, its driver, and selenium
+# --- code that only runs on import ---
+
+# install chromium, driver and selenium.
 import os
 os.system('apt update')
 os.system('apt install chromium-chromedriver')
 os.system('pip install selenium')
-# set options to be headless, ..
+import selenium
 from selenium import webdriver
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-# create a webdriver instance, ready to use
-wd = webdriver.Chrome('chromedriver',options=options)
-
-# make it easier to query and explore elements
-wd.select = wd.find_elements_by_css_selector
-wd.select1 = wd.find_element_by_css_selector
 from selenium.webdriver.remote.webelement import WebElement
-WebElement.__str__ = lambda self: self.get_attribute('outerHTML')
-WebElement.__repr__ = WebElement.__str__
-WebElement.select = WebElement.find_elements_by_css_selector
-WebElement.select1 = WebElement.find_element_by_css_selector
-WebElement.__getitem__ = WebElement.get_attribute
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+print('all module are loaded ')
 
-# show screenshot easily with _repr_png_
-def _screen_shot(self):
-    from tempfile import NamedTemporaryFile as TempFile
-    tmp = TempFile(suffix='.png')
-    self.save_screenshot(tmp.name)
-    return tmp.read()
-webdriver.Chrome._repr_png_ = _screen_shot
+# --- webdriver class that can be closed and restarted ---
+
+class WD(webdriver.Chrome):
+    ''' webdriver helper class'''
+
+    def __init__(self):
+        '''instantiate driver'''
+        
+        # set options to be headless, ..
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        
+        # options to conceal selenium activity on website - set to 
+        options.add_argument('--start-maximized')
+        options.add_argument('--start-fullscreen')
+        options.add_argument('--single-process')
+        options.add_argument('--incognito')
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_experimental_option('excludeSwitches', ["enable-automation"])
+        options.add_argument('disable-infobars')
+
+        # create a webdriver instance, ready to use
+        super().__init__('chromedriver', options=options)
+
+    # make it easier to query and explore elements 
+    def select(self, *args, **kwargs):
+        return self.find_elements(By.CSS_SELECTOR,*args, **kwargs)
+
+    def select1(self, *args, **kwargs):
+        return self.find_element(By.CSS_SELECTOR,*args, **kwargs)
+   
+    # show screenshot easily with _repr_png_
+    def _screen_shot(self):
+        from tempfile import NamedTemporaryFile as TempFile
+        tmp = TempFile(suffix='.png')
+        self.save_screenshot(tmp.name)
+        return tmp.read()
+        webdriver.Chrome._repr_png_ = _screen_shot
