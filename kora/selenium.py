02f6537639cd307fdd81e7508d2cb3d82048f012
Marfnl <marfnl@live.nl>
2022-03-01 15:47:31 +0100

    Update selenium.py
    Updated the pack with the select function

--- kora/selenium.py
+++ kora/selenium.py
@@ -11,6 +11,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
